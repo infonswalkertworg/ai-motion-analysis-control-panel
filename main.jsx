2962846bf8649aee00b8bc41239eb33cf5f74bdd
infonswalkertworg <info.nswalkertw.org@gmail.com>
2025-12-16 21:24:55 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Src/App.jsx';
-import './public/index.css'; // 如果你沒有 index.css，可以先刪掉這行
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
